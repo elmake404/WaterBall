7aa62ac6a28a87916c3cc76469f458387c11dd86
Valentin <kibirevvalik202@gmail.com>
2021-08-02 16:12:56 +0300

    [add] parctile

--- Assets/Scripts/Player/Gerridae.cs
+++ Assets/Scripts/Player/Gerridae.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Gerridae : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem _particleWaterTouch;
+    private PlayerMove _playerMove;
     private Rigidbody _rbMain;
     private List<GameObject> _waters = new List<GameObject>();
 
     private void Start()
     {
+        _playerMove = GetComponent<PlayerMove>();
         _rbMain = GetComponent<Rigidbody>();
     }
 
@@ -17,10 +21,23 @@ public class Gerridae : MonoBehaviour
         if (other.gameObject.layer == 4)
         {
             _waters.Add(other.gameObject);
+            if (!_rbMain.useGravity && _particleWaterTouch != null)
+                Instantiate(_particleWaterTouch, transform.position, transform.rotation);
 
             WaterTest();
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            if (_rbMain.velocity.y < 0 && !_playerMove.IsDrowning)
+            {
+                _rbMain.velocity = Vector3.Slerp(_rbMain.velocity, Vector3.zero, 0.7f);
+            }
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.layer == 4)
